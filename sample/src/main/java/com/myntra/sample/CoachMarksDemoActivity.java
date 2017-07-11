825c7fd32e195bd25a4c479c9233b8266e04551e
mudit.pant <mudit.pant@myntra.com>
2017-07-11 17:01:36 +0530

    Updated gradle, changed sample app

--- sample/src/main/java/com/myntra/sample/CoachMarksDemoActivity.java
+++ sample/src/main/java/com/myntra/sample/CoachMarksDemoActivity.java
@@ -3,8 +3,10 @@ package com.myntra.sample;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.myntra.coachmarks.PopUpCoachMark;
 import com.myntra.coachmarks.builder.CoachMarkBuilder;
@@ -16,24 +18,39 @@ import com.myntra.coachmarks.common.PopUpPosition;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CoachMarksDemoActivity extends AppCompatActivity {
 
-    private Button button;
+    @BindView(R.id.bt_show_coach_mark)
+    Button mShowCoachMarkButton;
+
+    @BindView(R.id.iv_sample_image)
+    ImageView mDemoImageView;
 
     private static final String FILE_NAME_MYNTRA_FONT_REGULAR = "Whitney-Book-Bas.otf";
     private static final String FILE_NAME_MYNTRA_FONT_BOLD = "Whitney-Semibold-Bas.otf";
+
+    private int mScreenHeight;
+    private int mScreenWidth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coachmarks_demo);
-        button = (Button) findViewById(R.id.bt);
+        ButterKnife.bind(this);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        mScreenHeight = displayMetrics.heightPixels;
+        mScreenWidth = displayMetrics.widthPixels;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        button.setOnClickListener(new View.OnClickListener() {
+        mShowCoachMarkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showCoachMark();
@@ -42,13 +59,13 @@ public class CoachMarksDemoActivity extends AppCompatActivity {
     }
 
     private void showCoachMark() {
-        Point anchorTop = new Point(0, 800);
-        Point anchorBottom = new Point(200, 1000);
-        InfoForViewToMask infoForViewToMask = InfoForViewToMask.create(new Point(500, 500), 800, 400).build();
+        Point anchorTop = new Point(0, 0);
+        Point anchorBottom = new Point(mScreenWidth, mScreenHeight);
+        InfoForViewToMask infoForViewToMask = InfoForViewToMask.create(new Point(0, 0), mScreenHeight, mScreenWidth).build();
         ArrayList<InfoForViewToMask> infoForViewToMaskArrayList = new ArrayList<>(1);
         infoForViewToMaskArrayList.add(infoForViewToMask);
         ImageLayoutInformation imageLayoutInformation = ImageLayoutInformation.create(R.dimen.image_width, R.dimen.image_height).build();
-        CoachMarkBuilder testBuilder = CoachMarkBuilder.create(anchorTop, anchorBottom, R.string.coach_mark_pdp_collections)
+        CoachMarkBuilder testBuilder = CoachMarkBuilder.create(anchorTop, anchorBottom, R.string.coach_mark_sample_message)
                 .setImageLayoutInformation(imageLayoutInformation)
                 .setUserDesiredPopUpPositionWithRespectToView(PopUpPosition.TOP)
                 .setPopUpCoachMarkDismissButtonPosition(DialogDismissButtonPosition.LEFT)
@@ -61,4 +78,6 @@ public class CoachMarksDemoActivity extends AppCompatActivity {
         PopUpCoachMark popUpCoachMark = PopUpCoachMark.newInstance(testBuilder);
         popUpCoachMark.show(getSupportFragmentManager(), "Test");
     }
+
+
 }
